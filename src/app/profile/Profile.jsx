eddb5165fbd4a41897da0f9e0c035e8b29847be6
kishan <guptakishan492@gmail.com>
2024-05-12 21:29:21 +0530

    minor

--- src/app/profile/Profile.jsx
+++ src/app/profile/Profile.jsx
@@ -16,7 +16,7 @@ const Profile = () => {
         router.back()
     }
     return (
-        <div className="px-2 lg:px-5">
+        <div className="px-2 lg:px-5 ">
   <div className=" px-2 lg:px-5 py-2">
           <WestIcon className="cursor-pointer" onClick={goBack} />
       </div>
